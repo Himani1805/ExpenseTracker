2a751fad44fb11b6cb0bcdd3df3036bc908e32b9
Himani Sharma <hinusharma18@gmail.com>
2025-09-22 12:11:43 +0530

    Add controller

---
+++ src/controllers/transction.conroller.js
@@ -0,0 +1,11 @@
+import Transction from "../models/transaction.model";
+
+export const addTransction= async(request, response)=>  {
+    try {
+        const {amount, type, category, descriptions} = request.body;
+
+        if()
+    } catch (error) {
+        
+    }
+}
--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,4 +1,11 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema{
-    amount:{ty
+const transactionSchema = new mongoose.Schema({
+    amount:{type:Number, required:true },
+    type:{type:String, enum: ["Credit", "Debit"], required:true },
+    category: {type:String,  required: true, trim: true},
+    description:{type:String, trim: true, required: true}
+}, {versionKey:false, timestamps:true})
+
+const Transction = mongoose.model("Transction", transactionSchema)
+export default Transction;
